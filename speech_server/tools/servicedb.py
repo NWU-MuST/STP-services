7718e67d574d4b385d6c03b11ef0d7b93e4e250c
Neil Kleynhans <ntkleynhans@gmail.com>
2017-02-20 21:08:03 +0200

    Added HTML service variants

--- speech_server/tools/servicedb.py
+++ speech_server/tools/servicedb.py
@@ -24,10 +24,12 @@ if __name__ == "__main__":
     db_curs = db_conn.cursor()
     db_curs.execute("CREATE TABLE services ( name VARCHAR(32) PRIMARY KEY, command VARCHAR (128) )")
     db_curs.executemany("INSERT INTO services (name, command) VALUES(?,?)",
-     [("diarize", "diarize.sh"), ("recognize","recognize.sh"), ("align","align.sh")])
+     [("diarize", "diarize.sh"), ("recognize","recognize.sh"), ("align","align.sh"),
+     ("diarize_html", "diarize_html.sh"), ("recognize_html","recognize_html.sh"), ("align_html","align_html.sh")])
 
     db_curs.execute("CREATE TABLE require ( name VARCHAR(32) PRIMARY KEY, audio VARCHAR(1), text VARCHAR(1) )")
-    require = [("diarize", "Y", "N"), ("recognize", "Y", "N"), ("align", "Y", "Y")]
+    require = [("diarize", "Y", "N"), ("recognize", "Y", "N"), ("align", "Y", "Y"),
+        ("diarize_html", "Y", "Y"), ("recognize_html", "Y", "Y"), ("align_html", "Y", "Y")]
     db_curs.executemany("INSERT INTO require(name, audio, text) VALUES(?,?,?)", require)
 
     db_curs.execute("CREATE TABLE diarize ( subsystem VARCHAR(32) PRIMARY KEY )")
@@ -38,6 +40,15 @@ if __name__ == "__main__":
 
     db_curs.execute("CREATE TABLE align ( subsystem VARCHAR(32) PRIMARY KEY )")
     db_curs.execute("INSERT INTO align(subsystem) VALUES(?)", ("en_ZA_16000",))
+
+    db_curs.execute("CREATE TABLE diarize_html ( subsystem VARCHAR(32) PRIMARY KEY )")
+    db_curs.execute("INSERT INTO diarize_html(subsystem) VALUES(?)", ("default",))
+
+    db_curs.execute("CREATE TABLE recognize_html ( subsystem VARCHAR(32) PRIMARY KEY )")
+    db_curs.execute("INSERT INTO recognize_html(subsystem) VALUES(?)", ("en_ZA_16000",))
+
+    db_curs.execute("CREATE TABLE align_html ( subsystem VARCHAR(32) PRIMARY KEY )")
+    db_curs.execute("INSERT INTO align_html(subsystem) VALUES(?)", ("en_ZA_16000",))
 
     db_conn.commit()
 
